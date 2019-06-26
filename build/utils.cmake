1e9d692b74291bd5f53c4ed5885406071e7eae83
Leah Lindner <hello@leah-lindner.com>
2019-06-26 01:51:36 +0100

    all projects listed by cmake

--- build/utils.cmake
+++ build/utils.cmake
@@ -105,3 +105,69 @@ function(target_definitions)
 		add_definitions(-DPLATFORM_x32)
 	endif()
 endfunction(target_definitions)
+
+
+# link to all dependancies
+###########################
+function(dependancyLinks TARGET)
+
+	set(dep_dir "${PROJECT_BINARY_DIR}/../dependancies")
+
+	if("${CMAKE_SIZEOF_VOID_P}" EQUAL "8")
+		set(dep_pf "${dep_dir}/x64")
+	 else() 
+		set(dep_pf "${dep_dir}/x32")
+	endif()
+
+	set(gtk_dbg "${dep_pf}/gtkmm/Debug/")
+	set(gtk_rel "${dep_pf}/gtkmm/Release/")
+
+	# separate debug and release libs
+	target_link_libraries (${TARGET} 		
+		debug ${dep_pf}/rttr/Debug/rttr_core_d.lib		optimized ${dep_pf}/rttr/Release/rttr_core.lib # RTTR
+
+		debug ${gtk_dbg}bz2d.lib				optimized ${gtk_rel}bbz2.lib			# GTK
+		debug ${gtk_dbg}cairod.lib				optimized ${gtk_rel}cairo.lib	
+		debug ${gtk_dbg}cairo-gobjectd.lib		optimized ${gtk_rel}cairo-gobject.lib	
+		debug ${gtk_dbg}freetyped.lib			optimized ${gtk_rel}freetype.lib	
+		debug ${gtk_dbg}libpng16d.lib			optimized ${gtk_rel}libpng16.lib	
+		debug ${gtk_dbg}pcre16d.lib				optimized ${gtk_rel}pcre16.lib	
+		debug ${gtk_dbg}pcre32d.lib				optimized ${gtk_rel}pcre32.lib	
+		debug ${gtk_dbg}pcrecppd.lib			optimized ${gtk_rel}pcrecpp.lib	
+		debug ${gtk_dbg}pcred.lib				optimized ${gtk_rel}pcre.lib	
+		debug ${gtk_dbg}pcreposixd.lib			optimized ${gtk_rel}pcreposix.lib	
+		debug ${gtk_dbg}pixman-1d.lib			optimized ${gtk_rel}pixman-1.lib	
+		debug ${gtk_dbg}zlibd.lib				optimized ${gtk_rel}zlib.lib	
+
+		debug ${gtk_dbg}atk-1.0.lib				optimized ${gtk_rel}atk-1.0.lib	 
+		debug ${gtk_dbg}aatkmm.lib				optimized ${gtk_rel}aatkmm.lib	 
+		debug ${gtk_dbg}acairomm-1.0.lib		optimized ${gtk_rel}acairomm-1.0.lib	 
+		debug ${gtk_dbg}aepoxy.lib				optimized ${gtk_rel}aepoxy.lib	 
+		debug ${gtk_dbg}aexpat.lib				optimized ${gtk_rel}aexpat.lib	 
+		debug ${gtk_dbg}afontconfig.lib			optimized ${gtk_rel}afontconfig.lib	 
+		debug ${gtk_dbg}agailutil-3.0.lib		optimized ${gtk_rel}agailutil-3.0.lib	 
+		debug ${gtk_dbg}agdk_pixbuf-2.0.lib		optimized ${gtk_rel}agdk_pixbuf-2.0.lib	 
+		debug ${gtk_dbg}agdk-3.0.lib			optimized ${gtk_rel}agdk-3.0.lib	 
+		debug ${gtk_dbg}agdkmm.lib				optimized ${gtk_rel}agdkmm.lib	 
+		debug ${gtk_dbg}agio-2.0.lib			optimized ${gtk_rel}agio-2.0.lib	 
+		debug ${gtk_dbg}agiomm.lib				optimized ${gtk_rel}agiomm.lib	 
+		debug ${gtk_dbg}aglib-2.0.lib			optimized ${gtk_rel}aglib-2.0.lib	 
+		debug ${gtk_dbg}aglibmm.lib				optimized ${gtk_rel}aglibmm.lib	 
+		debug ${gtk_dbg}agmodule-2.0.lib		optimized ${gtk_rel}agmodule-2.0.lib	 
+		debug ${gtk_dbg}agobject-2.0.lib		optimized ${gtk_rel}agobject-2.0.lib	 
+		debug ${gtk_dbg}agthread-2.0.lib		optimized ${gtk_rel}agthread-2.0.lib	 
+		debug ${gtk_dbg}agtk-3.0.lib			optimized ${gtk_rel}agtk-3.0.lib	 
+		debug ${gtk_dbg}agtkmm.lib				optimized ${gtk_rel}agtkmm.lib	 
+		debug ${gtk_dbg}aharfbuzz.lib			optimized ${gtk_rel}aharfbuzz.lib	 
+		debug ${gtk_dbg}alibcharset.lib			optimized ${gtk_rel}alibcharset.lib	 
+		debug ${gtk_dbg}alibffi.lib				optimized ${gtk_rel}alibffi.lib	 
+		debug ${gtk_dbg}alibiconv.lib			optimized ${gtk_rel}alibiconv.lib	 
+		debug ${gtk_dbg}alibintl.lib			optimized ${gtk_rel}alibintl.lib	 
+		debug ${gtk_dbg}apango-1.0.lib			optimized ${gtk_rel}apango-1.0.lib	 
+		debug ${gtk_dbg}apangocairo-1.0.lib		optimized ${gtk_rel}apangocairo-1.0.lib	 
+		debug ${gtk_dbg}apangoft2-1.0.lib		optimized ${gtk_rel}apangoft2-1.0.lib	 
+		debug ${gtk_dbg}apangomm.lib			optimized ${gtk_rel}apangomm.lib	 
+		debug ${gtk_dbg}apangowin32-1.0.lib		optimized ${gtk_rel}apangowin32-1.0.lib	 
+		debug ${gtk_dbg}asigc-2.0.lib			optimized ${gtk_rel}asigc-2.0.lib	 )
+
+endfunction(dependancyLinks)
